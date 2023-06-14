2594ba3d822aeed6ac9901d90e40b889efe63a24
hsadiq <ksadik320@gmail.com>
2023-06-14 13:11:52 +0530

    Added Async Nature code Using Node Compiler

--- timer.js
+++ timer.js
@@ -10,3 +10,5 @@ function showTime() {
     console. log("Activity-A: Trigerring Activity-B at "+showTime());
     setTimeout(showSessionExpire, 5000);
     console.log("Activity-A: Triggered Activity-B at "+showTime()+" will execute after 5 seconds");
+
+    //Uc-1 Completed
